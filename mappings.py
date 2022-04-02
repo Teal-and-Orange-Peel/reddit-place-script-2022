39138d5dd0d004d91cf2a9d2d86646242378301c
Boaz <bernieice11@gmail.com>
2022-04-02 20:56:32 +0300

    Add files via upload

--- mappings.py
+++ mappings.py
@@ -1,15 +1,23 @@
 color_map = {
-    "#FF4500": 2,  # bright red
-    "#FFA800": 3,  # orange
-    "#FFD635": 4,  # yellow
+    "#BE0039": 2,  # darkest red
+    "#FF4500": 3,  # orange
+    "#FFA800": 4,  # dark yellow
+    "#FFD635": 5,  # yellow
     "#00A368": 6,  # darker green
+    "#00CC78": 7,  # green
     "#7EED56": 8,  # lighter green
+    "#00756F": 9,  # darker blue
+    "#009EAA": 10,  # blue
     "#2450A4": 12,  # darkest blue
     "#3690EA": 13,  # medium normal blue
     "#51E9F4": 14,  # cyan
+    "#493AC1": 15,  # dark blue
+    "#6A5CFF": 16,  # lighter blue
     "#811E9F": 18,  # darkest purple
     "#B44AC0": 19,  # normal purple
+    "#FF3881": 22,  # hot pink
     "#FF99AA": 23,  # pink
+    "#6D482F": 24,  # dark  brown
     "#9C6926": 25,  # brown
     "#000000": 27,  # black
     "#898D90": 29,  # grey
@@ -21,15 +29,23 @@ color_map = {
 name_map = {
     2: "Bright Red",
     3: "Orange",
-    4: "Yellow",
+    4: "Dark Yellow",
+    5: "Yellow",
     6: "Dark Green",
+    7: "Green",
     8: "Light Green",
-    12: "Dark Blue",
-    13: "Blue",
+    9: "Darker Blue",
+    10: "Blue",
+    12: "darkest blue",
+    13: "medium normal blue",
     14: "Cyan",
+    15: "Dark Blue",
+    16: "Light34 Blue",
     18: "Dark Purple",
     19: "Purple",
+    22: "Hot Pink",
     23: "Pink",
+    24: "Dark Brown",
     25: "Brown",
     27: "Black",
     29: "Grey",
